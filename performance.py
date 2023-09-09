5a43a4f2541feaff07716195ff807b020afb2902
Test Env User <785515163@qq.com>
2023-09-09 13:37:07 +0800

    test unsplash dataset

--- performance.py
+++ performance.py
@@ -1,6 +1,7 @@
 import cv2
 import hashlib
 import os
+import pandas as pd
 import random
 import sys
 
@@ -35,13 +36,7 @@ class fake_server:
         alg.update(img.tobytes())
         return alg.hexdigest()
     
-    def init_server(self, img_path):
-        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-        h, w = img.shape
-        alg = hashlib.sha256()
-        alg.update(img.tobytes())
-        img_hash = alg.hexdigest()
-        
+    def init_dedup(self, img_hash, img_size):
         sh = img_hash[:fake_server.sh_len * 2]
         # add to index dic
         if sh not in self.sh_dic.keys():
@@ -50,7 +45,46 @@ class fake_server:
         elif img_hash not in self.init_file_dic.keys():
             self.sh_dic[sh] += 1
         # add to file dic
-        self.init_file_dic[img_hash] = h * w * 8
+        self.init_file_dic[img_hash] = img_size
+    
+    def init_server(self, img_path):
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        h, w = img.shape
+        alg = hashlib.sha256()
+        alg.update(img.tobytes())
+        img_hash = alg.hexdigest()
+        
+        self.init_dedup(img_hash, h * w * 8)
+
+    def dedup(self, img_hash, img_size):
+        # record communication overhead
+        sh = img_hash[:fake_server.sh_len * 2]
+        # sh not in sh_dic, file must not exist
+        if sh not in self.sh_dic.keys():
+            self.scheme_comm += img_size
+            self.lever_comm += img_size + fake_server.get_key_size()
+            self.odd_comm += img_size
+            # add sh and file into dic
+            self.sh_dic[sh] = 1
+            if img_hash not in self.init_file_dic.keys():
+                self.file_dic[img_hash] = img_size
+        # sh in sh_dic, verify whether file exists, and find file exists, then not upload
+        elif img_hash in self.file_dic.keys():
+            self.scheme_comm += img_size * self.sh_dic[sh]
+            self.lever_comm += (img_size + fake_server.get_key_size()) * self.sh_dic[sh]
+            self.odd_comm += img_size
+        # sh in sh_dic, verify whether file exists, and find file not exists, then should upload
+        else:
+            self.scheme_comm += img_size * self.sh_dic[sh]
+            self.lever_comm += (img_size + fake_server.get_key_size()) * (self.sh_dic[sh] + 1)
+            self.odd_comm += img_size
+            # add sh number
+            # add file into dic
+            self.sh_dic[sh] += 1
+            if img_hash not in self.init_file_dic.keys():
+                self.file_dic[img_hash] = img_size
+        # record storage overhead
+        self.ttl_size += img_size
     
     def upload(self, img_path):
         img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
@@ -59,34 +93,7 @@ class fake_server:
         alg.update(img.tobytes())
         img_hash = alg.hexdigest()
         
-        # record communication overhead
-        sh = img_hash[:fake_server.sh_len * 2]
-        # sh not in sh_dic, file must not exist
-        if sh not in self.sh_dic.keys():
-            self.scheme_comm += h * w * 8
-            self.lever_comm += h * w * 8 + fake_server.get_key_size()
-            self.odd_comm += h * w * 8
-            # add sh and file into dic
-            self.sh_dic[sh] = 1
-            if img_hash not in self.init_file_dic.keys():
-                self.file_dic[img_hash] = h * w * 8
-        # sh in sh_dic, verify whether file exists, and find file exists, then not upload
-        elif img_hash in self.file_dic.keys():
-            self.scheme_comm += h * w * 8 * self.sh_dic[sh]
-            self.lever_comm += (h * w * 8 + fake_server.get_key_size()) * self.sh_dic[sh]
-            self.odd_comm += h * w * 8
-        # sh in sh_dic, verify whether file exists, and find file not exists, then should upload
-        else:
-            self.scheme_comm += h * w * 8 * self.sh_dic[sh]
-            self.lever_comm += (h * w * 8 + fake_server.get_key_size()) * (self.sh_dic[sh] + 1)
-            self.odd_comm += h * w * 8
-            # add sh number
-            # add file into dic
-            self.sh_dic[sh] += 1
-            if img_hash not in self.init_file_dic.keys():
-                self.file_dic[img_hash] = h * w * 8
-        # record storage overhead
-        self.ttl_size += h * w * 8
+        self.dedup(img_hash, h * w * 8)
 
 def read_dir(dir_path : str) -> list:
     fps = []
@@ -97,11 +104,44 @@ def read_dir(dir_path : str) -> list:
                 break
     return fps
 
-def test_server(fps : list):
-    fcomm = open(out_dir + 'comm_KB.csv', 'w')
+def test_dedup(df : pd.DataFrame):
+    fcomm = open(out_dir + sys.argv[1] + '_comm_GB.csv', 'w')
     fcomm.write('exist_rate, scheme, lever, odd,\n')
 
-    fdedup = open(out_dir + 'dedup.csv', 'w')
+    fdedup = open(out_dir + sys.argv[1] + '_dedup.csv', 'w')
+    fdedup.write('exist_rate, scheme, lever, odd, ttl_size\n')
+    for exist_rate in range(11):
+        exist_rate /= 10
+        init_cnt = int(len(df) * exist_rate)
+        server = fake_server()
+        # select init_cnt files to init server
+        for i in range(init_cnt):
+            img_size = df.iloc[i]['photo_width'] * df.iloc[i]['photo_height'] * 8 * 3 * 0.006   # 0.006 is the compression ratio
+            img_hash = str(df.iloc[i]['blur_hash'])
+            server.init_dedup(img_hash, img_size)
+        # upload all files
+        for i in range(len(df)):
+            img_size = df.iloc[i]['photo_width'] * df.iloc[i]['photo_height'] * 8 * 3 * 0.006   # 0.006 is the compression ratio
+            img_hash = str(df.iloc[i]['blur_hash'])
+            server.dedup(img_hash, img_size)
+        fcomm.write(str(exist_rate) + ', ' + str(server.scheme_comm / 1024 / 1024 / 1024) + ', ' + str(server.lever_comm / 1024 / 1024 / 1024) + ', ' + str(server.odd_comm / 1024 / 1024 / 1024) + ',\n')
+        # compute deduplication rate
+        odd_strg = 0
+        # odd only store file content
+        for key in server.file_dic.keys():
+            odd_strg += server.file_dic[key]
+        # this scheme store file content and short hash
+        scheme_strg = odd_strg + server.sh_len * len(server.file_dic.keys())
+        # lever scheme store file content, short hash, and key
+        lever_strg = odd_strg + (server.sh_len + fake_server.get_key_size()) * len(server.file_dic.keys())
+        fdedup.write(str(exist_rate) + ', ' + str(scheme_strg) + ', ' + str(lever_strg) + ', ' + str(odd_strg) + ', ' + str(server.ttl_size) + ',\n')
+    fcomm.close()   
+
+def test_server(fps : list):
+    fcomm = open(out_dir + sys.argv[1] + '_comm_KB.csv', 'w')
+    fcomm.write('exist_rate, scheme, lever, odd,\n')
+
+    fdedup = open(out_dir + sys.argv[1] + '_dedup.csv', 'w')
     fdedup.write('exist_rate, scheme, lever, odd, ttl_size\n')
     for exist_rate in range(11):
         exist_rate /= 10
@@ -129,13 +169,16 @@ def test_server(fps : list):
 if __name__ == '__main__':
     if os.path.exists(out_dir) == False:
         os.makedirs(out_dir)
-    fps = []
     if sys.argv[1] == 'unsplash':
-        fps = read_dir(db_dir[0])
+        df = pd.read_csv(db_dir[0] + 'photos.tsv000', sep='\t', header=0, index_col=0)
+        df = df[:1000]
+        test_dedup(df)
+        print('Done the performance test in %s datasets with %d images!' % (sys.argv[1], len(df)))
     elif sys.argv[1] == 'sisp':
+        fps = []
         fps = read_dir(db_dir[1])
+        test_server(fps)
+        print('Done the performance test in %s datasets with %d images!' % (sys.argv[1], len(fps)))
     else:
         print('Error: wrong dataset name')
         exit(-1)
-    test_server(fps)
-    print('Done the performance test in %s datasets with %d images!' % (sys.argv[1], len(fps)))
